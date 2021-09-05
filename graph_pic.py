db16517e3a62561d3406183d6e7c3b342a2b2ece
Sergey Shakhbazov <shakhbazov@protei.ru>
2021-09-06 01:50:12 +0300

    simple method

--- graph_pic.py
+++ graph_pic.py
@@ -1,40 +1,48 @@
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import *
 
 from maze_templates import *
 
-class Example(Frame):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.master.title("Цвета")
-        self.pack(fill=BOTH, expand=1)
-
-        canvas = Canvas(self)
-
-        for i in c:
-
-            x, y = i[0], i[1]
-            x1 = 100+50*x
-            y1 = 350-50*y
-            x2 = x1+50
-            y2 = y1-50
-            canvas.create_rectangle(
-                x1, y1, x2, y2,
-                outline="#fb0", width=1, fill="#05f" #yellow
-            )
-            canvas.pack(fill=BOTH, expand=1)
+root = Tk()
+pics = Canvas(root, width=500, height=500, bg='white')
+pics.pack()
 
 
+for i in b:
+    x, y = i[0], i[1]
+    x1 = 100+50*x
+    y1 = 350-50*y
+    x2 = x1+50
+    y2 = y1-50
+    pics.create_rectangle(x1, y1, x2, y2,
+                       fill='gray',
+                       outline='black',
+                       width=2,
+                       activedash=(5, 4))
 
+x, y = zero_point_b[0],zero_point_b[1]
+x1 = 100 + 50 * x
+y1 = 350 - 50 * y
+x2 = x1 + 50
+y2 = y1 - 50
+pics.create_rectangle(x1, y1, x2, y2,
+                   fill='blue',
+                   outline='black',
+                   width=2,
+                   activedash=(5, 4))
 
-def main():
-    root = Tk()
-    ex = Example()
-    root.geometry("500x500+300+300")
-    root.mainloop()
+x, y = finish_b[0],finish_b[1]
+x1 = 100 + 50 * x
+y1 = 350 - 50 * y
+x2 = x1 + 50
+y2 = y1 - 50
+pics.create_rectangle(x1, y1, x2, y2,
+                   fill='green',
+                   outline='black',
+                   width=2,
+                   activedash=(5, 4))
 
-if __name__ == '__main__':
-    main()
+root.mainloop()
+
+#
+# if __name__ == '__main__':
+#     main()
