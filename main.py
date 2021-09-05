33b7ba2baa94e6cdd07141a04c7fb11277edc3b5
Sergey Shakhbazov <shakhbazov@protei.ru>
2021-09-06 00:48:00 +0300

    ref main

--- main.py
+++ main.py
@@ -33,28 +33,6 @@
 затраченному времени момент Мы с интересом просмотрим Ваше решение, даже если не все пункты будут выполнены. Также, если у Вас
 возникнут вопросы по условиям задания, то не стесняйтесь написать нам!'''
 
-import argparse
-from maze_templates import *
-import re
-
-
-maze_dict = {
-            'a':{'type':a, 'zero_p':zero_point_a, 'finish':finish_a},
-            'b':{'type':b, 'zero_p':zero_point_b, 'finish':finish_b},
-            'c':{'type':c, 'zero_p':zero_point_c, 'finish':finish_c},
-}
-
-parser = argparse.ArgumentParser(description='Great Description To Be Here')
-parser.add_argument('-w', '--way', dest= 'way', default='r,u', type=str, help='Введите шаги через запятую, например: l,r,u,d')
-parser.add_argument('-m', '--maze', dest= 'maze', default='a', choices = ['a', 'b', 'c'], type=str,
-                    help='Выберите тип лабиринта: a, b или c')
-args = parser.parse_args()
-
-way_list = re.findall('[u]|[r]|[l]|[d]', args.way)
-labyrinth = maze_dict[args.maze]
-zero_point = labyrinth['zero_p']
-finish = labyrinth['finish']
-
 def move(point, course, lab):
     x, y = point[0], point[1]
     if course == 'r' and (point[0]+1, point[1]) in lab:
@@ -78,6 +56,28 @@ def steper(lab, z_point, way):
         return steper(lab, new_position, way)
 
 if __name__ == "__main__":
+    import argparse
+    from maze_templates import *
+    import re
+
+    maze_dict = {
+        'a': {'type': a, 'zero_p': zero_point_a, 'finish': finish_a},
+        'b': {'type': b, 'zero_p': zero_point_b, 'finish': finish_b},
+        'c': {'type': c, 'zero_p': zero_point_c, 'finish': finish_c},
+    }
+
+    parser = argparse.ArgumentParser(description='Great Description To Be Here')
+    parser.add_argument('-w', '--way', dest='way', default='r,u', type=str,
+                        help='Введите шаги, например: l,r,u,d или  lrud')
+    parser.add_argument('-m', '--maze', dest='maze', default='a', choices=['a', 'b', 'c'], type=str,
+                        help='Выберите тип лабиринта: a, b или c')
+    args = parser.parse_args()
+
+    way_list = re.findall('[u]|[r]|[l]|[d]', args.way)
+    labyrinth = maze_dict[args.maze]
+    zero_point = labyrinth['zero_p']
+    finish = labyrinth['finish']
+
     print(labyrinth,'\n', zero_point, '\n', way_list)
 
     new_p = steper(labyrinth['type'], zero_point, way_list)
