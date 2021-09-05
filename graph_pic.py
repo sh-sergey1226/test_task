132d743708c632c6884ebb26d071a91796196a49
Sergey Shakhbazov <shakhbazov@protei.ru>
2021-09-06 01:26:08 +0300

    maze pics

--- graph_pic.py
+++ graph_pic.py
@@ -1,9 +1,6 @@
 from tkinter import Tk, Canvas, Frame, BOTH
 
-# labyrinth2 = ((0, 3), (1, 3), (2, 3), (3, 3),
-#               (0, 2), (f, f), (f, f), (3, 2),
-#               (0, 1), (f, f), (f, f), (3, 1),
-#               (0, 0), (1, 0), (f, f), (3, 0))
+from maze_templates import *
 
 class Example(Frame):
 
@@ -16,30 +13,28 @@ class Example(Frame):
         self.pack(fill=BOTH, expand=1)
 
         canvas = Canvas(self)
-        canvas.create_rectangle(
-            10, 10, 50, 50,
-            outline="#fb0", width=5, fill="#05f" #yellow
-        )
 
-        canvas.create_rectangle(
-            100, 100, 150, 150,
-            outline="#fb0", width=5, fill="#05f" #yellow
-        )
+        for i in c:
 
-        canvas.create_rectangle(
-            200, 200, 250, 250,
-            outline="#fb0", width=5, fill="#05f" #yellow
-        )
-        canvas.pack(fill=BOTH, expand=1)
+            x, y = i[0], i[1]
+            x1 = 100+50*x
+            y1 = 350-50*y
+            x2 = x1+50
+            y2 = y1-50
+            canvas.create_rectangle(
+                x1, y1, x2, y2,
+                outline="#fb0", width=1, fill="#05f" #yellow
+            )
+            canvas.pack(fill=BOTH, expand=1)
+
+
 
 
 def main():
     root = Tk()
     ex = Example()
-    root.geometry("1000x600+300+300")
+    root.geometry("500x500+300+300")
     root.mainloop()
-
-
 
 if __name__ == '__main__':
     main()
